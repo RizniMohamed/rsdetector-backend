d62519511e35e8ee44fa3802ddae9c9b83be99c9
Rizni <mnriznimohamed@gmail.com>
2023-10-07 04:18:01 +0530

    added jwt

---
+++ jwt.py
@@ -0,0 +1,16 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256"
+
+def create_new_token():
+    expiration = datetime.utcnow() + timedelta(minutes=30)
+    to_encode = {"exp": expiration}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI,UploadFile, File,WebSocket
+from fastapi import FastAPI, Header, HTTPException,UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
 from model import detection
 import cv2
 import numpy as np
-from skimage import exposure
-
+from jose import jwt
+from jwt import SECRET_KEY,ALGORITHM,create_new_token
 
 app = FastAPI() 
 
@@ -32,6 +31,36 @@ async def redirect_to_docs():
 @app.post("/V1/process") 
 async def process(file: UploadFile):
     
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    result = detection(img)
+    if result:
+        response = {"message": "{}".format(result)}
+    else: 
+        response = {"message": "No sign detected"}
+    return response
+
+
+@app.get("/V1/token")
+async def get_new_token():
+    token = create_new_token()
+    return {"access_token": token, "token_type": "bearer"}
+
+@app.post("/V1/secure-data")
+async def get_secure_data(file: UploadFile, authorization: str = Header(None)):
+    
+    if not authorization:
+        raise HTTPException(status_code=403, detail="Not authenticated")
+    token = authorization.split(" ")[1]
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except jwt.JWTError:
+        raise HTTPException(status_code=403, detail="Invalid token")
+
+
+
     contents = await file.read()
     nparr = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
